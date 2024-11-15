6ec48930b4cc82a8ba6464e65b48b6a87510d373
JamiezVo <tv2058@nyu.edu>
2024-11-14 23:47:53 -0500

    new changes

---
+++ Assets/SetXRRes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+// using OVR;
+
+
+public class SetXRRes : MonoBehaviour {
+    public float res;
+
+	void Start () {
+        //XRSettings.eyeTextureResolutionScale = res;
+    }
+	
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# sunflowerVR
-sunflower VR project
